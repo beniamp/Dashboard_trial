59c6889b35919e4488145d8d84bd92cca91d184d
beniamp <102410523+beniamp@users.noreply.github.com>
2024-08-03 15:48:09 +0330

    Update Dashboard_trial1.py

--- Dashboard_trial1.py
+++ Dashboard_trial1.py
@@ -83,6 +83,8 @@ st.set_page_config(layout='wide', initial_sidebar_state='expanded')
 with open('style (1).css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
 
+
+st.title('Order Management Dashboard')
 st.title("""
 <style>
     .reportview-container .sidebar {
